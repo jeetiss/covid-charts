1cfa8588d819e4b3c63b4dd049e972ee387510ce
jeetiss <jeetiss@yandex.ru>
2020-03-16 18:54:00 +0300

    fix: add legend

--- src/main.js
+++ src/main.js
@@ -30,13 +30,26 @@ const addStyles = () => {
     position: relative;
   }
 
-  .covid-19-title {
+  .covid-19-legend {
     position: absolute;
-    top: 8px;
-    left: 8px;
-    font-size: 24px;
-    font-weight: 600;
+    top: 2px;
+    left: 2px;
+    font-size: 12px;
     z-index: 10;
+  }
+
+  .covid-19-title {
+    font-size: 18px;
+    font-weight: 500;
+    padding-bottom: 8px;
+  }
+
+  .covid-19-confirmed {
+    color: red;
+  }
+
+  .covid-19-recovered {
+    color: #2196f3;
   }
 `)
   );
@@ -45,15 +58,42 @@ const addStyles = () => {
   return head.insertBefore(style, head.firstChild);
 };
 
-const wrapper = (element, text) => {
+const div = (className, text) => {
   let div = document.createElement("div");
-  div.classList.add("covid-19-title");
-  div.textContent = text;
+  div.classList.add(className);
 
+  if (text) {
+    div.textContent = text;
+  }
+
+  return div;
+};
+
+const last = arr => arr[arr.length - 1];
+
+const wrapper = (element, text) => {
   element.classList.add("covid-19-chart");
-  element.appendChild(div);
 
-  return element;
+  const confirmed = div("covid-19-confirmed");
+  const recovered = div("covid-19-recovered");
+  const deaths = div("covid-19-deaths");
+  const container = div("covid-19-chart-place");
+
+  const legend = div("covid-19-legend");
+  legend.appendChild(div("covid-19-title", text));
+  legend.appendChild(confirmed);
+  legend.appendChild(recovered);
+  legend.appendChild(deaths);
+
+  element.appendChild(legend);
+  element.appendChild(container);
+
+  return {
+    confirmed,
+    recovered,
+    deaths,
+    container
+  };
 };
 
 function createChart(
@@ -64,9 +104,29 @@ function createChart(
   let country = initialCountry;
 
   addStyles();
-  wrapper(element, country);
+  const { confirmed, recovered, deaths, container } = wrapper(
+    element,
+    `COVID-19 in ${country}`
+  );
 
-  const chart = tvChart(element, { width, height });
+  const chart = tvChart(container, {
+    width,
+    height,
+    localization: {
+      priceFormatter: price => Math.round(price).toString()
+    },
+    grid: {
+      horzLines: {
+        visible: false
+      },
+      vertLines: {
+        visible: false
+      }
+    },
+    handleScroll: {
+      vertTouchDrag: false
+    }
+  });
   const lineSeries = [
     chart.addLineSeries({ color: "red" }),
     chart.addLineSeries(),
@@ -84,6 +144,32 @@ function createChart(
   loadData().then(data => {
     loadedData = data;
     update(data);
+    setValues(last(data[country]));
+  });
+
+  const setValues = data => {
+    confirmed.textContent = "confirmed: " + data.confirmed;
+    recovered.textContent = "recovered: " + data.recovered;
+    deaths.textContent = "deaths: " + data.deaths;
+  };
+
+  chart.subscribeCrosshairMove(function(param) {
+    if (
+      param === undefined ||
+      param.time === undefined ||
+      param.point.x < 0 ||
+      param.point.x > width ||
+      param.point.y < 0 ||
+      param.point.y > height
+    ) {
+      loadedData && setValues(last(loadedData[country]));
+    } else {
+      setValues({
+        confirmed: param.seriesPrices.get(lineSeries[0]),
+        recovered: param.seriesPrices.get(lineSeries[1]),
+        deaths: param.seriesPrices.get(lineSeries[2])
+      });
+    }
   });
 
   return {
@@ -95,7 +181,5 @@ function createChart(
     countries: () => loadData().then(data => Object.keys(data))
   };
 }
-
-window.createChart = createChart;
 
 export default createChart;
